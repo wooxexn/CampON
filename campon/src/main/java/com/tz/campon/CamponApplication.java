f301835e9dd976b97c6a78342da284e583e5dbfa
전우선 <150002756+wooxexn@users.noreply.github.com>
2024-12-09 13:54:29 +0900

    캠핑장 조회 및 예약 서비스 웹사이트 프로젝트 campon 추가

---
+++ campon/src/main/java/com/tz/campon/CamponApplication.java
@@ -0,0 +1,13 @@
+package com.tz.campon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamponApplication.class, args);
+	}
+
+}
