355a98cc8ac9e41bf1dba40857dd2acff8a887ed
전우선 <150002756+wooxexn@users.noreply.github.com>
2024-12-10 15:45:16 +0900

    Resolve merge conflict in application.properties

--- campon/src/main/java/com/tz/campon/CamponApplication.java
+++ campon/src/main/java/com/tz/campon/CamponApplication.java
@@ -1,9 +1,11 @@
 package com.tz.campon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tz.campon.mapper")  // MyBatis 매퍼 스캔 추가
 public class CamponApplication {
 
 	public static void main(String[] args) {
