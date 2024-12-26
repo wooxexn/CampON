7655abf4f8d8fc5de9823975ed3ef8d6c3f66fe4
전우선 <jeonwooseon@naver.com>
2024-12-26 16:38:31 +0900

    헤더 로그인/로그아웃 표시를 위한 전역 로그인 상태 관리 구현

---
+++ src/main/java/com/tz/campon/common/config/GlobalControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.tz.campon.common.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("isLoggedIn")
+    public boolean isLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getPrincipal());
+    }
+}
